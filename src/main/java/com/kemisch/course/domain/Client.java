06e22a32b0ac359120ba12df69600ec7e4af1561
Leonardo Kemisch <leonardokemisch@gmail.com>
2022-02-06 17:12:22 -0300

    delete client

--- src/main/java/com/kemisch/course/domain/Client.java
+++ src/main/java/com/kemisch/course/domain/Client.java
@@ -24,7 +24,7 @@ public class Client implements Serializable {
     private String document;
     private Integer type;
 
-    @OneToMany(mappedBy = "client")
+    @OneToMany(mappedBy = "client", cascade = CascadeType.ALL)
     private List<Address> addresses = new ArrayList<>();
 
     @ElementCollection
