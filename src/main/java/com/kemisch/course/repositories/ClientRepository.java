9f8dea441e8349ac4808176dd8d2c78ecc8b3177
Leonardo Kemisch <leonardokemisch@gmail.com>
2022-05-24 19:06:36 -0300

    validations for Mail

--- src/main/java/com/kemisch/course/repositories/ClientRepository.java
+++ src/main/java/com/kemisch/course/repositories/ClientRepository.java
@@ -3,11 +3,11 @@ package com.kemisch.course.repositories;
 import com.kemisch.course.domain.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface ClientRepository extends JpaRepository<Client, Integer> {
 
-
-
-
+    @Transactional(readOnly = true)
+    Client findByMail(String email);
 }
--- src/main/java/com/kemisch/course/services/validation/ClientInsertValidator.java
+++ src/main/java/com/kemisch/course/services/validation/ClientInsertValidator.java
@@ -1,9 +1,12 @@
 package com.kemisch.course.services.validation;
 
+import com.kemisch.course.domain.Client;
 import com.kemisch.course.domain.enums.ClientType;
 import com.kemisch.course.dto.NewClientDTO;
+import com.kemisch.course.repositories.ClientRepository;
 import com.kemisch.course.resources.exceptions.FieldMessage;
 import com.kemisch.course.services.validation.utils.BR;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
@@ -11,6 +14,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ClientInsertValidator implements ConstraintValidator<ClientInsert, NewClientDTO> {
+
+    @Autowired
+    ClientRepository clientRepository;
+
     @Override
     public void initialize(ClientInsert ann) {
     }
@@ -25,6 +32,11 @@ public class ClientInsertValidator implements ConstraintValidator<ClientInsert, 
 
         if(objDto.getType().equals(ClientType.LEGAL_PERSON.getId()) && !BR.isValidCNPJ(objDto.getDocument())) {
             list.add(new FieldMessage("Document", "CNPJ Is Not Valid"));
+        }
+
+        Client aux = clientRepository.findByMail(objDto.getMail());
+        if(aux != null) {
+            list.add(new FieldMessage("Mail", "Mail already registered"));
         }
 
         for (FieldMessage e : list) {
